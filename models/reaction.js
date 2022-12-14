66a44e700a2df405d3b4e270e85c6868c0690326
James Prince <james.prince1@gmail.com>
2022-12-14 14:15:55 +1100

    Added reaction schema

---
+++ models/reaction.js
@@ -0,0 +1,27 @@
+const { Schema } = require("mongoose");
+const DateTime = require("luxon");
+
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        //TODO: test - Documentation reports Date.now returns milliseconds, this getter will need to be reviewed once data is in
+        get: v => DateTime.fromMillis(v),
+    }
+});
+
+
+module.export = reactionSchema;
