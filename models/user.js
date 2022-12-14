e345c1cac28d4962d66d4b9a77929adaee1d9fee
James Prince <james.prince1@gmail.com>
2022-12-14 13:25:33 +1100

    Added a user model. This will need to be tested with some seeds

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true, 
+        validator: {
+            validator: function (email) {
+                return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/i.test(email);
+            },
+            message:  "Please provide a valid email address",
+        }
+    },
+    thoughts: {
+        type: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought'
+            },
+        ],
+    },
+    friends: {
+        type: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ],
+    },
+});
+
+const User = model("user", userSchema);
+
+
+module.exports = User;
