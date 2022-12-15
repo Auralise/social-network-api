ddb0424ec8dc947aa9cf3f1cdb0866bd3b117fce
James Prince <james.prince1@gmail.com>
2022-12-15 13:55:18 +1100

    Added thought routes.

---
+++ routes/api/thoughts-routes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const {
+    getAllThoughts,
+    createThought,
+    getThoughtById,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require("../../controllers/thoughtsController");
+
+router.route("/").get(getAllThoughts).post(createThought);
+
+router.route("/:thoughtId").get(getThoughtById).put(updateThought).delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(createReaction).delete(deleteReaction);
+
+
+module.exports = router;
