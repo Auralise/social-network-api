55c13ac8ef5a027ec632b57b4fcafb31e2e8e75e
James Prince <james.prince1@gmail.com>
2022-12-15 12:35:40 +1100

    Added api route configuration.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
+const apiRoutes = require("./api");
+
+router.use("/api", apiRoutes);
 
 
-
-
+router.use("*", (req, res) => {
+    res.status(404).json({
+        message: "This route does not exists",
+    });
+});
 
 module.exports = router;
