f9df99cfe7d040893427349e33d25bb9474d7ff9
James Prince <james.prince1@gmail.com>
2022-12-19 15:30:42 +1100

    Added logic to allow updating the thought

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -88,6 +88,32 @@ const createThought = (req, res) => {
 
 const updateThought = (req, res) => {
 
+    if(!req.body.thoughtText){
+        res.status(400).json({
+            message: "Please include a thoughtText property in the body of the request",
+        });
+        return;
+    }
+
+    Thought.findByIdAndUpdate(req.params.thoughtId, {
+        thoughtText: req.body.thoughtText,
+    }, {
+        new: true
+    })
+    .then((thought)=> {
+        res.status(200).json({
+            message: "Thought successfully updated",
+            thought,
+        });
+    })
+    .catch((err)=>{
+        res.status(500).json({
+            message: "An internal server error occurred",
+            err
+        });
+    })
+
+
 };
 
 const deleteThought = (req, res) => {
